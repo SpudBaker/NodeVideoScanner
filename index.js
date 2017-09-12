1f15d86239d2be52aa44d9cca4715bd307c15f12
SpudBaker <spudbaker@gmail.com>
2017-09-12 19:51:53 +0100

    First Restful Service

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ app.get('/', function(request, response) {
   response.render('index');
 });
 
+app.get('/auth/login/:uniqueIdentifier', function (request, response) {
+  var user = {
+    firstName: 'Richard',
+    lastName: 'Baker'
+  }
+  var JSONdata = JSON.stringify(user);
+  response.end(JSONdata);
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
