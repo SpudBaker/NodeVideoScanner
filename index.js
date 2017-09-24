57b1cf98a8020f89dd0945a4a5e543e974fff939
SpudBaker <spudbaker@gmail.com>
2017-09-24 19:10:23 +0100

    modularised helper functions

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var app = express();
 var mySql = require('mysql');
+var helper = require('./lib/helperFunctions.js');
 var pool = mySql.createPool({
   connectionLimit : 5,
   host : 'ysp9sse09kl0tzxj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
@@ -11,11 +12,6 @@ var pool = mySql.createPool({
   database : 'q1uc6zz7ywr1i8n8'
 });
 
-function getDate() {
-  return new Date().toISOString().slice(0, 19).replace('T', ' ');
-}
-
-
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
@@ -23,7 +19,7 @@ app.get('/', (request, response) => {
   response.render('index');
 });
 
-require('./lib/rest-auth-login.js')(pool, app, getDate);
+require('./lib/rest-auth-login.js')(pool, app, helper);
 
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
