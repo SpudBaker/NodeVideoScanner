935d926d052ba48398f0bf6f6d5c37466cc6093a
SpudBaker <spudbaker@gmail.com>
2017-10-30 22:43:42 +0000

    rest-scan-videos service complete

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var app = express();
 var mySql = require('mysql');
 var helper = require('./lib/helperFunctions.js');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 var pool = mySql.createPool({
   connectionLimit : 5,
   host : 'ysp9sse09kl0tzxj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
@@ -17,7 +18,9 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // use it before all route definitions
-app.use(cors({origin: 'http://localhost:4200'}));
+app.use(cors({origin: ['http://localhost:4200','http://localhost:5000']}));
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/', (request, response) => {
   response.render('index');
@@ -25,6 +28,7 @@ app.get('/', (request, response) => {
 
 require('./lib/rest-auth-login.js')(pool, app, helper);
 require('./lib/rest-auth-validate-key.js')(pool, app, helper);
+require('./lib/rest-scan-videos.js')(pool, app, helper);
 
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
