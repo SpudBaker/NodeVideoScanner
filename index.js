5b44f1cc3e19bef3651a3326e0d4b12d728ee481
SpudBaker <spudbaker@gmail.com>
2017-11-05 09:23:12 +0000

    cors issue

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ var pool = mySql.createPool({
 app.set('port', (process.env.PORT || 5000));
 
 app.use(cors());
+app.options('*', cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
